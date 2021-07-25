9bf7444e91550897904306f32f34e14304a0ad88
tk1star2 <tk1star2@nate.com>
2021-07-25 20:35:21 +0900

    revised

--- new_docker.sh
+++ new_docker.sh
@@ -3,12 +3,12 @@ XSOCK=/tmp/.X11-unix
 XAUTH=/tmp/.docker.xauth
 XFILE=/run/user/1000
 
-TK_DATAPATH=/home/tk1star2/Desktop/DATA
+TK_DATAPATH=/home/tkay/Desktop/DATA
 TK_XAUTH=$XAUTHORITY
-TK_DOWNLOAD=/home/tk1star2/Downloads
-TK_USB=/media/tk1star2
-TK_WORKSPACE=/home/tk1star2/Desktop/CONTAINER_WORKSPACE
-DOCKER_HOME=/root/
+TK_DOWNLOAD=/home/tkay/Downloads
+TK_USB=/media/tkay
+TK_WORKSPACE=/home/tkay/Desktop/CONTAINER_WORKSPACE
+DOCKER_HOME=/root
 
 USER=tkay
 CONTAINER_NAME=tk_ENV1
@@ -17,9 +17,14 @@ echo "TK XAUTH is " $TK_XAUTH
 echo "TK DATAPATH is " $TK_DATAPATH
 
 nvidia-docker run --privileged=true \
+    --security-opt seccomp=tk_default.json \
+	--cap-add=SYS_ADMIN \
 	-d \
 	-p 6006:6006 \
-	-p 8888:8888\
+	-p 8888:8888 \
+    -p 8080:8080 \
+	-p 1000:22 \
+	-p 45555:1001 \
 	-e DISPLAY=$DISPLAY \
 	-e XAUTHORITY=$TK_AUTH \
 	-e XDG_RUNTIME_DIR=$XFILE \
@@ -28,6 +33,7 @@ nvidia-docker run --privileged=true \
 	--workdir=$DOCKER_HOME \
 	--ipc=host \
 	--shm-size=16g \
+	-v $XFILE:$XFILE:rw \
 	-v $XSOCK:$XSOCK:rw \
 	-v $TK_XAUTH:$XAUTH:rw \
 	-v $TK_DATAPATH:$DOCKER_HOME/DATASET:ro \
@@ -35,7 +41,7 @@ nvidia-docker run --privileged=true \
 	-v $TK_USB:$DOCKER_HOME/usb:rw \
 	-v $TK_WORKSPACE:$DOCKER_HOME/workspace:rw \
 	--name $CONTAINER_NAME \
-	-it nvidia/cuda:10.1-cudnn7-devel-ubuntu18.04 bash
+	-it nvidia/cuda:10.2-cudnn8-devel-ubuntu18.04 bash
 
 docker cp ./X11 $CONTAINER_NAME:$DOCKER_HOME
 docker cp ~/.vimrc $CONTAINER_NAME:$DOCKER_HOME
@@ -43,7 +49,7 @@ docker cp ~/.vim $CONTAINER_NAME:$DOCKER_HOME
 docker cp ~/.tmux.conf $CONTAINER_NAME:$DOCKER_HOME
 docker cp ./environment.txt $CONTAINER_NAME:$DOCKER_HOME
 sudo xhost +local:root
-#sudo cp /home/tk1star2/.Xauthority /root/
+#sudo cp /home/tkay/.Xauthority /root/
 #sudo xhost +local:root
 
 
--- restart_docker.sh
+++ restart_docker.sh
@@ -2,4 +2,5 @@ sudo xhost +local:root
 
 docker restart tk_ENV1
 docker exec -ti -e COLUMNS=$COLUMNS -e LINES=$LINES -e TERM=$TERM -e LC_ALL=C.UTF-8 tk_ENV1 bash
+#docker exec -ti -e COLUMNS=$COLUMNS -e LINES=$LINES -e TERM=$TERM -e LC_ALL=C.UTF-8 -u tkay tk_ENV1 bash
 
